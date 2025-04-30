5c769cbff6d160e1eea93870d7403d18413e1173
eisle <eisle@DESKTOP-35PF98K>
2025-04-30 12:22:24 +0200

    image is processed every 2 frame

--- yolo.py
+++ yolo.py
@@ -2,18 +2,8 @@ import cv2
 import torch
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")
-
-cap = cv2.VideoCapture('assets/face.mp4')
-
-while cap.isOpened():
-    ret, frame = cap.read()
-    
-    if not ret:
-        break
-
-    results = model(frame)
-
+def process_image(results):
+    boxes = []
     for result in results:
         for box in result.boxes:
             x1, y1, x2, y2 = map(int, box.xyxy[0])
@@ -21,12 +11,35 @@ while cap.isOpened():
             confidence = box.conf[0].item()
 
             if confidence > 0.5:
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 5)
-                cv2.putText(frame, f"{label} {confidence:.2f}", (x1+5, y1+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+                boxes.append((x1, y1, x2, y2, label, confidence))
+
+    return boxes
+
+model = YOLO("yolov8n.pt")
+
+cap = cv2.VideoCapture('assets/insane 4k.mp4')
+
+moment = 1
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    if moment % 2 == 0 or moment == 1:
+        results = model(frame)
+        boxes = process_image(results)
+
+    for x1, y1, x2, y2, label, confidence in boxes:
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 5)
+        cv2.putText(frame, f"{label} {confidence:.2f}", (x1+5, y1+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
     cv2.imshow("Frame", frame)
 
-    if cv2.waitKey(1) == ord('q'):
+    moment += 1
+
+    if cv2.waitKey(int(1)) == ord('q'):
         break
 
 cap.release()
